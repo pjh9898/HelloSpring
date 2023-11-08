4e4956df4d07c2545c30e46a84c7d7b8ec7397f9
pjh9898 <55084024+pjh9898@users.noreply.github.com>
2023-11-08 18:00:40 +0900

    중간 점검

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,34 @@
+package hello.core;
+
+import hello.core.Order.OrderService;
+import hello.core.Order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(getMemberRepository(), getDisCountPolicy());
+    }
+    @Bean
+    public static MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public static DiscountPolicy getDisCountPolicy() {
+        return new RateDiscountPolicy();
+    }
+}
